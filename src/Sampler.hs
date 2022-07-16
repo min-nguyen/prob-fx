349b8c70ecae8d5f1b15a620ffee705d75ec13f4
Minh Nguyen <minhnguyen1995@googlemail.com>
2022-07-16 11:23:53 +0100

    [Documentation] Sampler.hs

--- src/Sampler.hs
+++ src/Sampler.hs
@@ -3,7 +3,29 @@
 {- | An IO-based sampling monad
 -}
 
-module Sampler where
+module Sampler (
+  -- * Sampler monad
+    Sampler
+  , liftS
+  , sampleIO
+  , sampleIOFixed
+  , createSampler
+  -- * Sampling functions
+  -- $Sampling-functions
+  , sampleRandom
+  , sampleCauchy
+  , sampleNormal
+  , sampleUniform
+  , sampleDiscreteUniform
+  , sampleGamma
+  , sampleBeta
+  , sampleBernoulli
+  , sampleBinomial
+  , sampleCategorical
+  , sampleDiscrete
+  , samplePoisson
+  , sampleDirichlet
+  ) where
 
 import Control.Monad ( replicateM )
 import Control.Monad.ST (ST, runST, stToIO)
@@ -26,62 +48,98 @@ import System.Random.MWC ( initialize )
 newtype Sampler a = Sampler {runSampler :: ReaderT MWC.GenIO IO a}
   deriving (Functor, Applicative, Monad)
 
+-- | Lift an @IO@ computation into @Sampler@
 liftS :: IO a -> Sampler a
 liftS f = Sampler $ lift f
 
--- | Takes a Sampler, provides it a random generator, and runs the sampler in the IO context
+-- | Takes a @Sampler@, provides it a random generator, and runs the sampler in the @IO@ context
 sampleIO :: Sampler a -> IO a
 sampleIO m = MWC.createSystemRandom >>= (runReaderT . runSampler) m
 
--- | Takes a Sampler, provides it a fixed generator, and runs the sampler in the IO context
+-- | Takes a @Sampler@, provides it a fixed generator, and runs the sampler in the @IO@ context
 sampleIOFixed :: Sampler a -> IO a
 sampleIOFixed m = MWC.create >>= (runReaderT . runSampler) m
 
-sampleIOFixedSeed :: Int -> Sampler a -> IO a
-sampleIOFixedSeed n m = initialize (V.singleton (fromIntegral n :: Word32)) >>= (runReaderT . runSampler) m
+-- | Takes a @Sampler@, provides it a custom fixed generator, and runs the sampler in the @IO@ context
+sampleIOCustom :: Int -> Sampler a -> IO a
+sampleIOCustom n m = initialize (V.singleton (fromIntegral n :: Word32)) >>= (runReaderT . runSampler) m
 
--- | Takes a distribution which awaits a generator, and returns a Sampler
+-- | Takes a distribution which awaits a generator, and returns a @Sampler@
 createSampler :: (MWC.GenIO -> IO a) -> Sampler a
 createSampler f = Sampler $ ask >>= lift . f
 
--- *** IO-based sampling functions
+{- $Sampling-functions
+Given their distribution parameters, these functions await a generator and then sample a value from the distribution in the @IO@ monad
+-}
 
--- | Given distribution parameters, these functions await a generator and then sample a value from a distribution
-sampleRandom :: MWC.GenIO -> IO Double
+sampleRandom 
+  :: MWC.GenIO 
+  -> IO Double
 sampleRandom = \gen -> MWC.uniform gen
 
-sampleCauchy :: Double -> Double -> (MWC.GenIO -> IO Double)
+sampleCauchy 
+  :: Double -- ^ Location
+  -> Double -- ^ Scale
+  -> (MWC.GenIO -> IO Double)
 sampleCauchy μ σ = \gen -> genContVar (cauchyDistribution μ σ) gen
 
-sampleNormal :: Double -> Double -> (MWC.GenIO -> IO Double)
+sampleNormal 
+  :: Double -- ^ Mean
+  -> Double -- ^ Standard deviation
+  -> (MWC.GenIO -> IO Double)
 sampleNormal μ σ = \gen -> MWC.Dist.normal μ σ gen
 
-sampleUniform :: Double -> Double -> (MWC.GenIO -> IO Double)
+sampleUniform 
+  :: Double -- ^ Lower-bound
+  -> Double -- ^ Upper-bound
+  -> (MWC.GenIO -> IO Double)
 sampleUniform min max = \gen -> MWC.uniformR (min, max) gen
 
-sampleDiscreteUniform :: Int -> Int -> (MWC.GenIO -> IO Int)
+sampleDiscreteUniform 
+  :: Int -- ^ Lower-bound
+  -> Int -- ^ Upper-bound
+  -> (MWC.GenIO -> IO Int)
 sampleDiscreteUniform min max = \gen -> MWC.uniformR (min, max) gen
 
-sampleGamma :: Double -> Double -> (MWC.GenIO -> IO Double)
+sampleGamma 
+  :: Double -- ^ Shape k
+  -> Double -- ^ Scale θ
+  -> (MWC.GenIO -> IO Double)
 sampleGamma k θ = \gen -> MWC.Dist.gamma k θ gen
 
-sampleBeta :: Double -> Double -> (MWC.GenIO -> IO Double)
+sampleBeta 
+  :: Double -- ^ Shape α
+  -> Double -- ^ Shape β
+  -> (MWC.GenIO -> IO Double)
 sampleBeta α β = \gen -> MWC.Dist.beta α β gen
 
-sampleBernoulli :: Double -> (MWC.GenIO -> IO Bool)
+sampleBernoulli 
+  :: Double -- ^ Probability of @True@
+  -> (MWC.GenIO -> IO Bool)
 sampleBernoulli p = \gen -> MWC.Dist.bernoulli p gen
 
-sampleBinomial :: Int -> Double -> (MWC.GenIO -> IO [Bool])
+sampleBinomial 
+  :: Int    -- ^ Number of trials
+  -> Double -- ^ Probability of successful trial
+  -> (MWC.GenIO -> IO [Bool])
 sampleBinomial n p = \gen -> replicateM n (MWC.Dist.bernoulli p gen)
 
-sampleCategorical :: V.Vector Double -> (MWC.GenIO -> IO Int)
+sampleCategorical 
+  :: V.Vector Double -- ^ Probabilities
+  -> (MWC.GenIO -> IO Int)
 sampleCategorical ps =  \gen -> MWC.Dist.categorical (ps) gen
 
-sampleDiscrete :: [Double] -> (MWC.GenIO -> IO Int)
+sampleDiscrete 
+  :: [Double] -- ^ Probabilities
+  -> (MWC.GenIO -> IO Int)
 sampleDiscrete ps = \gen -> MWC.Dist.categorical (V.fromList ps) gen
 
-samplePoisson :: Double -> (MWC.GenIO -> IO Int)
+samplePoisson 
+  :: Double   -- ^ Rate λ
+  -> (MWC.GenIO -> IO Int)
 samplePoisson λ = \gen -> MWC.Probability.sample (MWC.Probability.poisson λ) gen
 
-sampleDirichlet :: [Double] -> (MWC.GenIO -> IO [Double])
+sampleDirichlet 
+  :: [Double] -- ^ Concentrations
+  -> (MWC.GenIO -> IO [Double])
 sampleDirichlet xs = \gen -> MWC.Dist.dirichlet xs gen
