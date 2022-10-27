42047dfec3b6a523c9d3d29e66835567dd5dae4b
Oliver Killane <oliverkillane@gmail.com>
2022-10-27 15:23:48 +0100

    Removed safe head (listToMaybe) and safe tail (drop 1)

--- src/Util.hs
+++ src/Util.hs
@@ -1,15 +1,3 @@
 {- | Some minor utility functions -}
 
-module Util (
-    safeHead
-  , safeTail) where
-
--- | Safely attempt to return the head of a list
-safeHead :: [a] -> Maybe a
-safeHead []     = Nothing
-safeHead (x:xs) = Just x
-
--- | Return the tail of a list, behaving as the identity function upon an empty list
-safeTail :: [a] -> [a]
-safeTail [] = []
-safeTail (x:xs) = xs
+module Util () where
